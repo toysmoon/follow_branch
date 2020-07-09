5a7539071022c6adaeac95a1b9fee24ec364abe0
Sungjung Jo <toysmoon@gmail.com>
2020-07-09 18:06:00 +0900

    initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Follow branch
+
+Autometically merge branch when followed branch is pushed
---
+++ index.js
@@ -0,0 +1,15 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+const token = core.getInput('github-token', { required: true });
+const base = core.getInput('target-branch', { required: true });
+const head = core.getInput('following-branch', { required: true });
+
+const octokit = new github.GitHub(token);
+const context = github.context;
+
+const { owner, repo } = context.repo;
+
+octokit.pulls.merge({ owner, repo, base, head }).catch((error) => {
+  core.setFailed(`Failed to merge branch: ${error}`);
+});
